9a58f35fd909cc48ef9e0f0222318f7860405735
BruhVal <134472168+BruhVal@users.noreply.github.com>
2023-06-16 16:41:29 +0800

    Sprites

---
+++ SPRITE GP/Assets/SCRIPT/playermovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.PlayerLoop;
+
+
+public class playermovement : MonoBehaviour
+{
+    //Variable for our speed modifier 
+    public float moveSpeed;
+
+    //variable for our input
+    public Vector2 movementInput;
+
+    //Variable For our rigitbody2d
+    public Rigidbody2D rigidBody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //Handles physics
+    private void FixedUpdate()
+    {
+        //adds a velocity on our rigidbody
+        rigidBody.velocity = movementInput * moveSpeed;
+    }
+
+    private void OnMove(InputValue inputValue)
+    {
+        movementInput = inputValue.Get<Vector2>();
+    }
+
+
+
+}
